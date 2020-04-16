6e5ab8043bd8531d037b5fea71bd9fad2dce000e
Vinay Vadali <venkatvinay2211@gmail.com>
2020-04-16 18:50:16 +0530

    get server port from env variable

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express')
 const { db } = require('./db')
 const todoRoute = require('./routes/todos')
 const app = express()
+
+const SERVER_PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/', express.static(__dirname + '/public'))
 app.use('/todos', todoRoute)
 db.sync()
   .then(() => {
-    app.listen(6540)
+    app.listen(SERVER_PORT)
   })
   .catch((err) => {
     console.error(err)
