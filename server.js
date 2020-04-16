616597dad1a4e890b5292adcae249f8a9f527199
Vinay Vadali <venkatvinay2211@gmail.com>
2020-04-16 18:14:36 +0530

    ädded server connectivity file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { db } = require('./db')
+const todoRoute = require('./routes/todos')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/', express.static(__dirname + '/public'))
+app.use('/todos', todoRoute)
+db.sync()
+  .then(() => {
+    app.listen(6540)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
